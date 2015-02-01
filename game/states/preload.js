bb8c14721b763fbeb7d643738c39b63fa482e55f
Adam Wentz <awentz@theonion.com>
2015-02-01 14:03:14 -0600

    Goodbye, yeoman

--- game/states/preload.js
+++ game/states/preload.js
@@ -12,7 +12,6 @@ Preload.prototype = {
 
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
-    this.load.image('yeoman', 'assets/yeoman-logo.png');
 
   },
   create: function() {
